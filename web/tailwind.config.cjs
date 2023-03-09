eb789beaa5f249143a4482f9338f6a8af86d8016
Benjamin Babic <11808448+b-babic@users.noreply.github.com>
2023-03-09 16:25:20 +0100

    feat(web): add sane defaults

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,6 +8,13 @@ module.exports = {
       fontFamily: {
         sans: ["Source Sans Pro", "sans-serif"],
         serif: ["Alegreya", "serif"],
+      },
+      colors: {
+        ...defaultTheme.colors, // TODO: Remove when colors are customized
+        black: "#353534",
+        sand: {
+          300: "#ECECE8",
+        },
       },
     },
   },
